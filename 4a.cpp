4a62e9dc429109dcfdb17158771fc33fc3884e6b
mob1324 <113995933+mob1324@users.noreply.github.com>
2022-10-04 13:14:12 +0300

    4a.cpp

---
+++ 4a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a;
+	cin >> a;
+	if (a == 2) {
+		cout << ("NO"); return 0;
+	}
+	if (a % 2 == 0) {
+		cout << ("YES");
+	}
+	else {
+		cout << ("NO");
+	}
+	return 0;
+}
